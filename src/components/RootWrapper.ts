cf0907c6709c4644b2e153a0ecc74498be4d3bfb
ninjastack <ninjastack101@gmail.com>
2019-01-06 10:21:51 +0530

    Move wrapper from app to RootWrapper component

---
+++ src/components/RootWrapper.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const RootWrapper = styled('div')`
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+`;
+
+export default RootWrapper;
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
-import InputButton from '../components/InputButton';
-import GlobalStyles from '../global-styles';
 
-const Wrapper = styled('div')`
-  height: 100%;
-  width: 100%;
-  margin: 0;
-  padding: 0;
-`;
+import InputButton from '../components/InputButton';
+import RootWrapper from '../components/RootWrapper';
+import GlobalStyles from '../global-styles';
 
 class App extends React.Component {
   render() {
     return (
-      <Wrapper>
+      <RootWrapper>
         <InputButton />
         <GlobalStyles />
-      </Wrapper>
+      </RootWrapper>
     );
   }
 }
