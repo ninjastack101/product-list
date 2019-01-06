372fbf6e1fe7bab942fccb7a893150dc1bfaac01
ninjastack <ninjastack101@gmail.com>
2019-01-06 17:25:31 +0530

    Update reducer to handle fetch products api actions

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,10 +1,54 @@
 import { AnyAction } from "redux";
 import { IAppReducerState } from '../types';
+import {
+  FETCH_PRODUCTS_LIST_FAILURE,
+  FETCH_PRODUCTS_LIST_REQUEST,
+  FETCH_PRODUCTS_LIST_SUCCESS,
+} from '../actions/actionTypes';
 
-export const initialState: IAppReducerState = {};
+export const initialState: IAppReducerState = {
+  products: {
+    error: undefined,
+    values: [],
+    isLoading: false,
+  },
+};
 
 const reducer = (state = initialState, action: AnyAction) => {
   switch(action.type) {
+    case FETCH_PRODUCTS_LIST_REQUEST:
+      return {
+        ...state,
+        products: {
+          ...state.products,
+          isLoading: true,
+          values: [],
+          error: undefined,
+        },
+      };
+
+    case FETCH_PRODUCTS_LIST_SUCCESS:
+      return {
+        ...state,
+        products: {
+          ...state.products,
+          isLoading: false,
+          values: action.data.results,
+          error: undefined,
+        },
+      };
+
+    case FETCH_PRODUCTS_LIST_FAILURE:
+      return {
+        ...state,
+        products: {
+          ...state.products,
+          isLoading: false,
+          values: [],
+          error: action.error,
+        },
+      };
+
     default:
       return state;
   }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,18 @@
 
+export interface IProducts {
+
+};
+
+export interface IProducts {
+  values: IProduct[],
+  error: any;
+  isLoading: boolean;
+};
+
 // Application Reducer state type
-export interface IAppReducerState {};
+export interface IAppReducerState {
+  products: IProducts;
+};
 
 // Application Root State
 export interface IApplicationRootState {
