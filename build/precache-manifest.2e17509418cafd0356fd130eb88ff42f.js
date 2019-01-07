077fb7dc99d16871c052ae8dd9760989686d5ea3
ninjastack <ninjastack101@gmail.com>
2019-01-07 11:34:02 +0530

    Using pre-build source to ignore heroku memory limit exceeding while compiling

---
+++ build/precache-manifest.2e17509418cafd0356fd130eb88ff42f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbdf0edcc2431bcf206a",
+    "url": "/static/js/main.dbdf0edc.chunk.js"
+  },
+  {
+    "revision": "c625e8ba9a22d04e0871",
+    "url": "/static/js/1.c625e8ba.chunk.js"
+  },
+  {
+    "revision": "80108a814ce08f121aef04079bf08f26",
+    "url": "/index.html"
+  }
+];
