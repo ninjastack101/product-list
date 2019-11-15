b1661002baf5f7cff6bb2e21873a392d91443786
ninjastack <ninjastack101@gmail.com>
2019-11-15 13:39:48 +0530

    Added unit test case for reducers

---
+++ src/reducers/reducers.test.ts
@@ -0,0 +1,80 @@
+import * as actions from '../actions/actionTypes';
+import { IAppReducerState } from '../types';
+import reducer from './index';
+
+describe('fetchProducts', () => {
+
+  let initaction: any = {};
+
+  let initialState: IAppReducerState = {
+    products: {
+      error: undefined,
+      values: [],
+      isLoading: false,
+    },
+  };
+
+  it('should return the initial state', done => {
+    expect(reducer(undefined, initaction)).toEqual(initialState);
+    done();
+  });
+
+  it('should handle FETCH_PRODUCTS_LIST_REQUEST', done => {
+
+    const startAction = {
+      type: actions.FETCH_PRODUCTS_LIST_REQUEST,
+    }
+
+    const expectObj = {
+      products: {
+        isLoading: true,
+        values: [],
+        error: undefined,
+      },
+    };
+    expect(reducer(initialState, startAction)).toEqual(expectObj);
+    done();
+  });
+
+  it('should handle FETCH_PRODUCTS_LIST_SUCCESS', done => {
+
+    const actionSuccess = {
+      type: actions.FETCH_PRODUCTS_LIST_SUCCESS,
+      data: {
+        results: {
+          id: "category",
+          name: "Categories",
+          type: "text"
+        },
+      },
+    };
+
+    const expectObj = {
+      products: {
+        isLoading: false,
+        values: actionSuccess.data.results,
+        error: undefined,
+      },
+    };
+    expect(reducer(initialState, actionSuccess)).toEqual(expectObj);
+    done();
+  });
+
+  it('should handle FETCH_PRODUCTS_LIST_FAILURE', done => {
+
+    const failAction = {
+      type: actions.FETCH_PRODUCTS_LIST_FAILURE,
+      error: 'unable to fetch product list'
+    }
+
+    const expectObj = {
+      products: {
+        isLoading: false,
+        values: [],
+        error: failAction.error,
+      },
+    };
+    expect(reducer(initialState, failAction)).toEqual(expectObj);
+    done();
+  });
+});
